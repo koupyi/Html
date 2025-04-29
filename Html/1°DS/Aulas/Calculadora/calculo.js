7542903ac71f7d2cde9120dbbe12f0a305992cc0
koupyi <hector.oliveira5@etec.sp.gov.br>
2025-04-29 14:53:30 -0300

    ajuda

--- Html/1°DS/Aulas/Calculadora/calculo.js
+++
@@ -1,7 +0,0 @@
-function somar (){
-    var x = 2
-    var y = 2
-    let res = x + y
-    document.getElementById("soma").innerHTML=res
-    document.getElementById("conf-soma").innerHTML="Calculado ✔"
-}
